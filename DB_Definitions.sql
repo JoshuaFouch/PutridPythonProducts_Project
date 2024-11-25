45402bf305f752634f090a6a2b7a8de9d4a9a23a
Joshua Fouch <162926985+JoshuaFouch@users.noreply.github.com>
2024-11-25 18:44:15 -0500

    Update DB_Definitions.sql

--- DB_Definitions.sql
+++ DB_Definitions.sql
@@ -42,50 +42,52 @@ drop table if exists Factories;
 create table Factories (
   Location varchar(255) primary key,
   Factory_Name varchar(255) not null,
-  Category_Specialty varchar(100) default 'Varied'
+  Category_Specialty varchar(100) default 'Varied' --defaults to varied if none given
     check (Category_Specialty in ('Chemical', 'Tool', 'Purification', 'Varied'))
 );
 
 /*Managers*/
 create table Managers (
-  ID int primary key,
+  ID int primary key identity (20000, 1) check (ID <= 29999), --starts at 2000, increments by 1, cannot exceed 7 digits
   Name varchar(255) not null,
-  Age int check (Age >= 0),
-  Salary bigint check (Salary >= 0),
-  Hire_Date datetime default getdate(),
+  Age int check ((Age >= 23) and (Age <= 80)) --must be 23-80 years old
+  Salary bigint check (Salary >= 0), --nonnegative constraint
+  Hire_Date datetime default getdate(), --defaults to date of insertion
   Factory varchar(255),
   foreign key (Factory) references Factories (Location)
 );
 
 /*Employees*/
 create table Employees (
-    ID int primary key,
+    ID int primary key identity (30000, 1) check (ID <= 39999),
     Name varchar(255) not null,
-    Age int check (Age >= 0),
-    Salary bigint check (Salary >= 0),
+    Age int check ((Age >= 14) and (Age <= 80)), --must be 14-80 years old
+    Salary bigint check (Salary >= 0), --nonnegative constraint
     Works_At varchar(255),
     Manager int,
-    Role varchar(255) default 'Varied'
+    Role varchar(255) default 'Varied' --defaults to varied if none given
     	check (Role in ('Assembly Worker', 'Janitor', 'Secretary', 'Marketing', 'IT', 'Sales', 'Varied')),
     foreign key (Works_At) references Factories (Location),
     foreign key (Manager) references Managers (ID),
-    unique (ID, Manager) /*one boss per employee*/
+    unique (ID, Manager) --one boss per employee
 );
 
 /*Boss*/
 create table Boss (
   Manager_ID int,
-  Employee_ID int unique, /*makes sure employee only has 1 boss*/
+  Employee_ID int unique, --makes sure employee only has 1 boss
   foreign key (Manager_ID) references Managers (ID) on delete cascade on update cascade,
+    --makes sure to delete tuple if said employee or manager is deleted
   foreign key (Employee_ID) references Employees (ID) on delete cascade on update cascade
+    --makes sure to delete tuple if said employee or manager is deleted
 );
 
 /*Products*/
 create table Products (
-  ID int primary key,
+  ID int primary key identity (40000, 1) check (ID <= 49999),
   Name varchar(255) not null,
-  Stock int check (Stock >= 0),
-  Price int check (Price >= 0),
+  Stock int check (Stock >= 0), --nonnegative constraint
+  Price int check (Price >= 0), --nonnegative constraint
   Category varchar(255)
   	check (Category in ('Chemical', 'Tool', 'Purification', 'Varied'))
 );
@@ -94,25 +96,25 @@ create table Products (
 create table Makes (
   Factory_Location varchar(255),
   Product_ID int,
-  Quality_Grade int check ((Quality_Grade >= 0) and (Quality_Grade <= 10)),
-  Num_Produced int check (Num_Produced >= 0),
+  Quality_Grade int check ((Quality_Grade >= 0) and (Quality_Grade <= 10)), --must be a number 1-10
+  Num_Produced int check (Num_Produced >= 0), --nonnegative constraint
   foreign key (Factory_Location) references Factories (Location),
   foreign key (Product_ID) references Products (ID)
 );
 
 /*Customers*/
 create table Customers (
-  ID int primary key,
+  ID int primary key identity (50000, 1) check (ID <= 59999),
   Name varchar(255),
   Credit_card bigint
 );
 
 /*Customer_Orders*/
 create table Customer_Orders (
-  ID int primary key,
+  ID int primary key identity (1000000, 1) check (ID <= 9999999),
   Customer_ID int,
-  Order_Date datetime default getdate(),
-  Total_Price int check (Total_Price >= 0),
+  Order_Date datetime default getdate(), --defaults to date of insertion
+  Total_Price int check (Total_Price > 0), --must have bought something
   foreign key (Customer_ID) references Customers (ID)
 );
 
@@ -121,7 +123,7 @@ create table Customer_Order_Details (
   Order_ID int,
   Customer_ID int,
   Product_ID int,
-  Amount_Bought int check (Amount_Bought >= 0),
+  Amount_Bought int check (Amount_Bought > 0), --must have bought something
   foreign key (Order_ID) references Customer_Orders (ID),
   foreign key (Customer_ID) references Customers (ID),
   foreign key (Product_ID) references Products (ID)
@@ -129,27 +131,29 @@ create table Customer_Order_Details (
 
 /*Vendors*/
 create table Vendors (
-  ID int primary key,
+  ID int primary key identity (60000, 1) check (ID <= 69999),
   Name varchar(255) not null,
   Partner int,
   foreign key (Partner) references Managers (ID),
-  unique (ID, Partner) /*one partner per vendor*/
+  unique (ID, Partner) --one partner per vendor
 );
 
 /*Partners*/
 create table Partners (
   Manager_ID int,
-  Vendor_ID int unique, /*ensures only one partner per vendor*/
+  Vendor_ID int unique, --ensures only one partner per vendor
   foreign key (Manager_ID) references Managers (ID) on delete cascade on update cascade,
+    --makes sure deletes tuple if said manager or vendor is deleted
   foreign key (Vendor_ID) references Vendors (ID) on delete cascade on update cascade
+    --makes sure deletes tuple if said manager or vendor is deleted
 );
 
 /*Vendor_Orders*/
 create table Vendor_Orders (
-  ID int primary key,
+  ID int primary key identity (2000000, 1) check (ID <= 2999999),
   Vendor_ID int,
-  Order_Date datetime default getdate(),
-  Total_Price int,
+  Order_Date datetime default getdate(), --defaults to date of insertion
+  Total_Price int check (Total_Price > 0), --must have bought something
   foreign key (Vendor_ID) references Vendors (ID)
 );
 
@@ -158,7 +162,7 @@ create table Vendor_Order_Details (
   Order_ID int,
   Vendor_ID int,
   Product_ID int,
-  Amount_bought int check (Amount_Bought >= 0),
+  Amount_bought int check (Amount_Bought >= 0), --must have bought something
   foreign key (Order_ID) references Vendor_Orders (ID),
   foreign key (Vendor_ID) references Vendors (ID),
   foreign key (Product_ID) references Products (ID)
